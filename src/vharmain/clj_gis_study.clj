e17469db3dce153037bdcb12245fee1551567e31
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2020-10-18 14:45:36 +0300

    Experiment world map with oz

--- src/vharmain/clj_gis_study.clj
+++ src/vharmain/clj_gis_study.clj
@@ -1,5 +1,13 @@
 (ns vharmain.clj-gis-study
+  (:require [oz.core :as oz])
   (:gen-class))
+
+(comment
+  (oz/start-server!))
+
+(def world (oz/load "resources/world-map.json"))
+
+(oz/view! world :mode :vega)
 
 (defn -main
   "I don't do a whole lot ... yet."
